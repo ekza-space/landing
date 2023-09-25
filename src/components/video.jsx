b5e0f397d01e2ef6a445c7587fb37d9d5dd6f9fc
Dmitry <sobolevd@foamline.com>
2023-09-25 19:09:45 +0800

    improve subscription section for vertical phones

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -4,18 +4,20 @@ import Subscribe from "./subscribe";
 
 export default function Video() {
   return (
-    <section className="relative">
-      <div className="absolute inset-0 flex justify-center items-center">
-        <Subscribe />
-      </div>
-      {isIOS || isSafari || isFirefox || true ? (
-        <img className="mt-4" src="/img/moon.jpg" alt="Moon Image" />
-      ) : (
-        <video className="mt-4" autoPlay loop muted playsInline>
-          <source src="/video/enter.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      )}
-    </section>
+    <div className="h-screen">
+      <section className="relative">
+        <div className="absolute inset-0 flex justify-center customPadding">
+          <Subscribe />
+        </div>
+        {isIOS || isSafari || isFirefox || true ? (
+          <img className="mt-4 object-cover h-screen w-screen" src="/img/moon.jpg" alt="Moon Image" />
+        ) : (
+          <video className="mt-4" autoPlay loop muted playsInline>
+            <source src="/video/enter.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        )}
+      </section>
+    </div>
   );
 }
